16d1e9870c286828760e37425bef7c6ca6f1381a
Rafael Ortiz <sumercedrafa@gmail.com>
2021-12-19 19:12:54 +0100

    Updated SaveWService, Removed .vscode from git

--- lib/features/weather/save_weather_service.dart
+++ lib/features/weather/save_weather_service.dart
@@ -3,55 +3,26 @@ import 'package:multiple_result/multiple_result.dart';
 import '../../features/features.dart';
 import '../../core/core.dart';
 
-final weatherServiceProvider = Provider<WeatherService>((ref) {
-  final weatherRepository = ref.watch(weatherRepositoryProvider);
+
+final saveWeatherServiceProvider = Provider<SaveWeatherService>((ref) {
   final sharedPreferencesService = ref.watch(sharedPreferencesServiceProvider);
 
-  return OpenWeatherService(
-    weatherRepository: weatherRepository,
+  return SharedPreferencesSaveWeatherService(
     sharedPreferencesService: sharedPreferencesService,
   );
 });
 
-abstract class WeatherService {
-  Future<Result<Failure, List<WeatherModel>>> getLondonFiveDayForecast();
-  Future<Result<Failure, WeatherModel>> getLondonWeather();
+abstract class SaveWeatherService {
   Future<Result<Failure, bool>> saveWeatherLocally(WeatherModel weatherModel);
   Future<Result<Failure, bool>> saveForecastLocally(
       List<WeatherModel> forecast);
 }
 
-class OpenWeatherService implements WeatherService {
-  const OpenWeatherService(
-      {required this.weatherRepository,
-      required this.sharedPreferencesService});
-  final WeatherRepository weatherRepository;
+class SharedPreferencesSaveWeatherService implements SaveWeatherService {
+  const SharedPreferencesSaveWeatherService(
+      {required this.sharedPreferencesService});
+
   final SharedPreferencesService sharedPreferencesService;
-
-  @override
-  Future<Result<Failure, List<WeatherModel>>> getLondonFiveDayForecast() async {
-    try {
-      final forecastEntitiesList =
-          await weatherRepository.getLondonFiveDayForecast();
-      final mappedList = forecastEntitiesList
-          .map((forecast) => WeatherModel.fromEntity(forecast))
-          .toList();
-      return Success(mappedList);
-    } on Failure catch (failure) {
-      return Error(failure);
-    }
-  }
-
-  @override
-  Future<Result<Failure, WeatherModel>> getLondonWeather() async {
-    try {
-      final currentWeather = await weatherRepository.getLondonWeather();
-      final mappedWeather = WeatherModel.fromEntity(currentWeather);
-      return Success(mappedWeather);
-    } on Failure catch (failure) {
-      return Error(failure);
-    }
-  }
 
   @override
   Future<Result<Failure, bool>> saveForecastLocally(
